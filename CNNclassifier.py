994fbeba7675bddb6a8089a337b7143554419ad9
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-09-03 00:50:03 +0200

    building the cnn model

--- CNNclassifier.py
+++ CNNclassifier.py
@@ -15,8 +15,41 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 ############ DEFINING THE CLASSES ###########
-class_names = ['cat', 'dog', 'butterfly', 'balloon', 'car', 'tiger', 'lamp', 'rose']
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+############## Visualizing Some training images from the dataset ########
+plt.figure(figsize=(8,8))
+for i in range(10):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(True)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+####################### BUILDING A CUSTOMISED CNN MODEL #############
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10)
+
+
+    
+    
+    ])
+            
+    
 
 
 
 
+
+
+    
