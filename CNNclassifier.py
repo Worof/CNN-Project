2fd3e47050a35f021fdbe9c2fbd97fb8073b5a95
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-09-03 01:41:40 +0200

    tuning hyperparameters

--- CNNclassifier.py
+++ CNNclassifier.py
@@ -1,4 +1,5 @@
 ############ IMPORTING NECESSARY LIBRARIES #########################
+from tabnanny import verbose
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
@@ -30,21 +31,98 @@ for i in range(10):
 plt.show()
 
 ####################### BUILDING A CUSTOMISED CNN MODEL #############
-model = models.Sequential([
-    layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
-    layers.MaxPooling2D((2,2)),
-    layers.Conv2D(64, (3,3), activation='relu'),
-    layers.MaxPooling2D((2,2)),
-    layers.Conv2D(64, (3,3), activation='relu'),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(10)
+def model_build(hp):
+    model = models.Sequential()
+    model.add(layers.Conv2D(hp.Int('conv1_units', min_value=32, max_value=128, step=16), (3,3), activation='relu', input_shape=(32,32,3)))
+    model.add(layers.MaxPooling2D((2,2)))
+    model.add(layers.Conv2D(hp.Int('conv2_units', min_value=32, max_value=128, step=16), (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2)))
+    model.add(layers.Conv2D(hp.Int('conv3_units', min_value=32, max_value=128, step=16), (3,3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(hp.Int('dense_units', min_value=32, max_value=128, step=16), activation='relu'))
+    model.add(layers.Dropout(hp.Float('dropout_rate', min_value=0.0, max_value=0.5, step=0.1)))
+    model.add(layers.Dense(10))
+    ##############PRINTING THE MODEL SUMMARY ###############
+    model.summary()
+    ###############COMPILING & Optimizing THE CNN MODEL ###############
+    optimizer = tf.keras.optimizers.Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4]))
+    
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
+    return model
+
+#############DEFINING THE TUNER ###############
+tuner = RandomSearch(
+    build_model,
+    objective='val_accuracy',
+    max_trials=10,
+    executions_per_trial=1,
+    directory='my_dir',
+    project_name='cifar10_tunning'
+    
+    )
+tuner.search(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+##############TRAINING THE MODEL###############
+model = tuner.hypermodel.build(best_hps)
+history = model.fit(train_images, train_labels, epochs = 15, validation_data = (test_images, test_labels))
+
+##################EVALUATING THE MODEL###################
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose =2)
+print(f'\n Test accuracy is: {test_acc}')
+
+##################### PLOTTING THE ACCURACY AND VALIDATION VALUES #################
+
+plt.figure(figsize=(8,6))
+
+plt.subplot(1,2,1)
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.ylim([0,1])
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+plt.grid(True)
+
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.ylim([0,1])
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.grid(True)
 
 
-    
-    
-    ])
-            
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
